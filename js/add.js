94c24e24d48e86f1c8a4469aae0861650cf6746f
Abdelrahman Mohamed <11410120210229@stud.cu.edu.eg>
2023-04-12 01:37:48 +0200

    update v5

--- js/add.js
+++ js/add.js
@@ -1,3 +1,14 @@
-function submited(){
+function submited() {
   window.alert("Employee Added");
+}
+let clicked = false;
+function open_list() {
+  if (!clicked){
+    document.getElementById('small-links').style.display = "flex";
+    clicked = true;
+  }
+  else{
+    document.getElementById('small-links').style.display = "none";
+    clicked = false;
+  }
 }
